411009f1b26c3aa856af8c3adbef73c00e02a1f8
alinurd <ali.mnrdn14005@gamail.com>
2026-03-08 17:02:40 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { VideoPreview } from '@/app/components/VideoPreview';
 import { HeroSection } from '@/app/components/sections/HeroSection';
 import { LoveStorySection } from '@/app/components/sections/LoveStorySection';
@@ -10,53 +10,35 @@ import { AudioControl } from './components/AudioControl';
 import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const guestName = searchParams.get('to')?.replace(/-/g, ' ').replace(/\bdan\b/gi, '&') || '';
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MainContent guestName={guestName} />
+    </Suspense>
+  );
+}
+
+function MainContent({ guestName }: { guestName: string }) {
   const [showVideoPreview, setShowVideoPreview] = useState(true);
-  const [showAllSection, setShowAllSection] = useState(true);
   const [videoUrl, setVideoUrl] = useState('/videos/vid.mp4');
+  const [showAllSection, setShowAllSection] = useState(true);
 
-   const searchParams = useSearchParams();
-    const guestName = (() => {
-    const to = searchParams.get('to');
-    if (!to) return '';
-  
-    return to
-      .replace(/-/g, ' ')
-      .replace(/\bdan\b/gi, '&');
-  })();
-
-  // Detect device
   useEffect(() => {
     const isMobile = window.matchMedia('(max-width: 768px)').matches;
-
-    if (isMobile) {
-      setVideoUrl('/videos/vid1.mp4');
-    } else {
-      setVideoUrl('/videos/vid.mp4');
-    }
+    if (isMobile) setVideoUrl('/videos/vid1.mp4');
   }, []);
 
-  const handleVideoComplete = () => {
-    setShowVideoPreview(false);
-  };
-
-  const handleOpenInvitation = () => {
-    setShowAllSection(true);
-  };
+  const handleVideoComplete = () => setShowVideoPreview(false);
+  const handleOpenInvitation = () => setShowAllSection(true);
 
   return (
     <>
-      {showVideoPreview && (
-        <VideoPreview
-          onComplete={handleVideoComplete}
-          videoUrl={videoUrl}
-          guestName={guestName}
-        />
-      )}
-
+      {showVideoPreview && <VideoPreview videoUrl={videoUrl} guestName={guestName} onComplete={handleVideoComplete} />}
       {!showVideoPreview && (
         <main className="w-full overflow-x-hidden relative">
           <HeroSection openInvitation={handleOpenInvitation} />
-
           {showAllSection && (
             <>
               <LoveStorySection />
@@ -64,7 +46,6 @@ export default function Home() {
               <GuestWishesSection />
             </>
           )}
-
           <div className="fixed bottom-6 right-6 z-[999]">
             <AudioControl />
           </div>
